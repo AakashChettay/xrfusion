4e71b9fe92477245c485da599d4a509214df9302
Chettay Aakash <127005500+AakashChettay@users.noreply.github.com>
2024-03-19 22:20:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -182,3 +182,17 @@ function updateTime() {
 }
 
 setInterval(updateTime, 1000);
+
+
+// popup 
+const popup =document.querySelector('.popup');
+const close =document.querySelector('.close');
+window.onload = function(){
+setTimeout(function(){
+popup.style.display = "block"
+}, 2000)
+}
+close.addEventListener('click', () => 
+{
+     popup.style.display = "none"; 
+    })
